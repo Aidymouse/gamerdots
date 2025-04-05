04ba85f94bb1715ac5456c5e4018cb41a9fb5aca
Aidan <aidan@thewizardscat.co.nz>
2025-04-05 23:17:54 +1300

    [Sway] theming

---
+++ sway/themes/stoic.d
@@ -0,0 +1,15 @@
+
+# class                 border  bground text    indicator child_border
+
+# Green
+client.focused          #333333 #333333 #f2f2f2 $aqua     $aqua
+client.focused_inactive #000000 #000000 $fg     #484E50   #5F676A
+client.unfocused        #000000 #000000 #888888 #292D2E   #222222
+client.urgent           #2F343A #CC241D $fg_1   #CC241D   #CC241D
+client.placeholder      #1D2021 #0C0C0C $fg     #000000   #0C0C0C
+
+# Keeps title bar but gets rid of menu
+default_border normal 0 
+
+gaps inner 5px
+gaps outer 5px
