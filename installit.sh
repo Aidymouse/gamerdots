5138b4a56f5ef426b2b8b323b43534b0a09d4d21
Aidan <aidymouse4891@gmail.com>
2026-05-06 19:50:04 +1200

    install it script

--- installit.sh
+++ installit.sh
@@ -1,1 +1,3 @@
 sudo apt install grim slurp wl-clipboard lazygit;
+# Makes messing with audio not hot garbage (but i still dont know how to tell my audio card which hdmi output to use)
+sudo apt install gpwgraph;
