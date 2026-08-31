35cb4501f16fd0965f07a9c448fca4401bb568ef
Aidan <aidymouse4891@gmail.com>
2026-08-31 22:18:03 +1200

    install it comments

--- installit.sh
+++ installit.sh
@@ -1,3 +1,6 @@
+# Installit script last used on Ubuntu 25.05 or some shit
+
+# Screenshot capability
 sudo apt install grim slurp wl-clipboard lazygit;
 # Makes messing with audio not hot garbage (but i still dont know how to tell my audio card which hdmi output to use)
 sudo apt install gpwgraph;
