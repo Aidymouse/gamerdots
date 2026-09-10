0801d0b0b7f7014287136a328e5e9711ada2932f
Aidan Archer <aidymouse4891@gmail.com>
2026-09-11 00:02:26 +1200

    update config for set up on new machine

---
+++ archit.sh
@@ -0,0 +1,8 @@
+# Sway stuff
+sudo pacman -S waybar wofi;
+# Git
+sudo pacman -S lazygit;
+# Screenshotting
+sudo pacman -S grim slurp wl-clipboard;
+# Oh my ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
